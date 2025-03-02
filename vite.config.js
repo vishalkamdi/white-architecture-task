df7bae6fd43fe54963936db35d1760ddd76be5a1
Vishal Kamdi <vishalkamdi2901@gmail.com>
2025-03-02 10:02:04 +0000

    FEAT: GIthub action flow.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
+// next.config.js
+const isProd = process.env.NODE_ENV === "production";
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // base: isProd ? "/vishalkamdi" : "/",
+  build: {
+    assetsDir: isProd ? "/vishalkamdi" : "/",
+  }
+
 })
